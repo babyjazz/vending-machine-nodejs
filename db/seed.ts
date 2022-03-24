5175e5d793a1369aae0399bce8591e54421d4574
jitiwattana robru <jitiwattana@satangcorp.com>
2022-03-24 15:21:26 +0700

    update seed data

--- db/seed.ts
+++ db/seed.ts
@@ -15,14 +15,64 @@ const userData = [
   },
 ]
 
+const productData = [
+  {
+    name: 'Coke',
+    price: 13,
+    amount: 7,
+  },
+  {
+    name: 'Bento',
+    price: 25,
+    amount: 8,
+  },
+  {
+    name: 'Lays',
+    price: 20,
+    amount: 10,
+  },
+]
+
 async function main() {
+  console.log('Clear data before seed ...')
+  await prisma.orders.deleteMany()
+  await prisma.products.deleteMany()
+  await prisma.wallets.deleteMany()
+  await prisma.users.deleteMany()
+  console.log('Cleared data every tables')
+
   console.log(`Start seeding ...`)
+  // Create users
   for (let i = 0; i < userData.length; i++) {
-    const u = userData[i]
+    const data = userData[i]
     await prisma.users.create({
-      data: u,
+      data,
     })
   }
+  // Create wallets
+  for (let i = 0; i < productData.length; i++) {
+    const data = productData[i]
+    await prisma.products.create({
+      data,
+    })
+  }
+  // Create wallet per user
+  const users = await prisma.users.findMany()
+  users.forEach(async (user) => {
+    await prisma.wallets.create({
+      data: {
+        user_id: user.id,
+        one: 20,
+        five: 20,
+        ten: 20,
+        twenty: 20,
+        fifty: 20,
+        hundred: 20,
+        five_hundred: 20,
+        thounsand: 20,
+      },
+    })
+  })
   console.log(`Seeding finished.`)
 }
 
